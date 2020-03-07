2d030f0106cbc1bbff1c656f7e5b0ecc73746ae0
Bruno Bradach <bruno.bradach@conexiogroup.com>
2020-03-07 12:11:31 -0300

    Cleanup after start using ngx-element

--- src/app/components/sponsor/sponsor.module.ts
+++ src/app/components/sponsor/sponsor.module.ts
@@ -1,29 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, Type, Injector } from '@angular/core';
-
+import { NgModule, Type } from '@angular/core';
 import { SponsorComponent } from './sponsor.component';
-import { NgxElementService } from 'ngx-element';
 
 @NgModule({
-  declarations: [
-    SponsorComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    SponsorComponent
-  ],
-  entryComponents: [
-    SponsorComponent
-  ]
+  declarations: [SponsorComponent],
+  imports: [CommonModule],
+  exports: [SponsorComponent],
+  entryComponents: [SponsorComponent]
 })
 export class SponsorModule {
   customElementComponent: Type<any> = SponsorComponent;
 
-  constructor(private injector: Injector, private ngxElementService: NgxElementService) {
-    this.ngxElementService.receiveContext(SponsorComponent, injector);
-  }
+  constructor() { }
 
   ngDoBootstrap() { }
 }
--- src/app/components/talk/talk.module.ts
+++ src/app/components/talk/talk.module.ts
@@ -1,29 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, Type, Injector } from '@angular/core';
-
+import { NgModule, Type } from '@angular/core';
 import { TalkComponent } from './talk.component';
-import { NgxElementService } from 'ngx-element';
 
 @NgModule({
-  declarations: [
-    TalkComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    TalkComponent
-  ],
-  entryComponents: [
-    TalkComponent
-  ]
+  declarations: [TalkComponent],
+  imports: [CommonModule],
+  exports: [TalkComponent],
+  entryComponents: [TalkComponent]
 })
 export class TalkModule {
   customElementComponent: Type<any> = TalkComponent;
 
-  constructor(private injector: Injector, private ngxElementService: NgxElementService) {
-    this.ngxElementService.receiveContext(TalkComponent, injector);
-  }
+  constructor() { }
 
   ngDoBootstrap() { }
 }
